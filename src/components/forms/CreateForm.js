9eb2d0c6181dd889a14ae53e934303aba7decefe
Luis Montero <luismontero@LAAPJG5H355-LUIS.local>
2019-04-20 14:38:23 -0600

    Controlled form with Hook State

--- src/components/forms/CreateForm.js
+++ src/components/forms/CreateForm.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Form, Input } from 'semantic-ui-react'
 
+
+
 const CreateForm = () => {
+    const [formData, setFormData] = useState({
+        name: ""
+    })
+    const handleChange = (event)=> setFormData({name: event.target.value})
   return (
     <Form>
     <Form.Field required>
       <label>Path Name</label>
-      <Input placeholder='React Developer path' />
+      <Input value={formData.name} placeholder='React Developer path' onChange={handleChange} />
     </Form.Field>
-    <Form.Button content='Add the path' />
+    <Form.Button disabled={formData.name.length <= 0} content='Add the path' />
   </Form>
   )
 }
