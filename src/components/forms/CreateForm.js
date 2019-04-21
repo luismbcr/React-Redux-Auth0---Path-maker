15c72629bca07e851812fe693a4cd08e89cb9911
Luis Montero <luismontero@LAAPJG5H355-LUIS.local>
2019-04-21 16:44:02 -0600

    Handle description field

--- src/components/forms/CreateForm.js
+++ src/components/forms/CreateForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input} from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as pathActions from "../../actions/path"
@@ -7,26 +7,38 @@ import * as pathActions from "../../actions/path"
 const CreateForm = (props) => {
     const [formData, setFormData] = useState({
         name: "",
+        description: "",
         isValid: false
     })
-    const handleChange = (event)=> {
+    const handleChangeTitle = (event)=> {
+        console.log(event.target);
         const {value} = event.target;
         const isValidForm  = (/^(?=.*[^\W_])[\w ]*$/).test(value) && value.length > 2;
         setFormData({name: value, isValid: isValidForm})
     }
+    const handleChangeDescription = (event)=> {
+      
+      const {value} = event.target;
+      console.log(value);
+      const isValidForm  = (/^(?=.*[^\W_])[\w ]*$/).test(formData.name) && value.length > 2;
+      setFormData({name: formData.name, description: value, isValid: isValidForm})
+  }
     const handleSubmit = ()=>{
       props.addPath({
-        "title": formData.name
+        "title": formData.name,
+        "description": formData.description
       })
-      setFormData({name: "", isValid: false})
+      setFormData({name: "",description:"", isValid: false})
     }
+
     return (
       <Form onSubmit={handleSubmit}>
       <Form.Field required>
         <label>Path Name</label>
-        <Input value={formData.name} placeholder='React Developer path' onChange={handleChange} />
+        <Input value={formData.name} placeholder='React Developer path' onChange={handleChangeTitle} />
         <p>Please enter a valid name, only letters and numbers</p>
       </Form.Field>
+      <Form.TextArea required label='What is your goal?' placeholder='Tell us more about your goal' value={formData.description} onChange={handleChangeDescription} />
       <Form.Button disabled={!formData.isValid} content='Add the path' />
     </Form>
     )
